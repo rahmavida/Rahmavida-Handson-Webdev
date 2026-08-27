0337943664cec91dbe9e3f2f00a8a9cf78ee6509
rahmavida <vidarahma154@gmail.com>
2026-08-27 22:15:28 +0700

    up

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-// Tulis JS DOM manipulation kamu di sini!
-
 // === Quest 1 ===
 
 // 1. Ambil logo (class="logo")
+const logo = document.querySelector('.logo');
 
 // 2. Klik logo → alert (contoh: alert('Selamat datang!'))
+if (logo) {
+    logo.addEventListener('click', () => {
+        alert('Selamat datang!');
+    });
+}
+
 
 // === Quest 2 ===
 
 // 3. Ambil semua kartu (.card)
+const cards = document.querySelectorAll('.card');
 
 // 4. Klik kartu → alert isi kartunya (hint: card.querySelector('p').textContent)
+cards.forEach((card) => {
+    card.addEventListener('click', () => {
+        const textContent = card.querySelector('p').textContent;
+        alert(textContent);
+    });
+});
